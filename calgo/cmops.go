dee3a219ef8928533c4a0721fc5ce1e57d0a1acf
Harri Rautila <harri.rautila@gmail.com>
2013-01-20 21:03:27 +0200

    Versions for transposed A or B.

--- calgo/cmops.go
+++ calgo/cmops.go
@@ -26,7 +26,7 @@ func MultAligned(C, A, B []float64, alpha, beta float64, ldC, ldA, ldB, P, S, L,
     Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
     Bm.step = C.int(ldB)
 
-    C.mult_mdata_aligned_notrans(
+    C.dmult_aligned_notrans(
         (*C.mdata_t)(unsafe.Pointer(&Cm)),
         (*C.mdata_t)(unsafe.Pointer(&Am)),
         (*C.mdata_t)(unsafe.Pointer(&Bm)),
@@ -47,7 +47,7 @@ func MultUnAligned(C, A, B []float64, alpha, beta float64, ldC, ldA, ldB, P, S, 
     Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
     Bm.step = C.int(ldB)
 
-    C.mult_mdata_unaligned_notrans(
+    C.dmult_unaligned_notrans(
         (*C.mdata_t)(unsafe.Pointer(&Cm)),
         (*C.mdata_t)(unsafe.Pointer(&Am)),
         (*C.mdata_t)(unsafe.Pointer(&Bm)),
@@ -55,27 +55,6 @@ func MultUnAligned(C, A, B []float64, alpha, beta float64, ldC, ldA, ldB, P, S, 
         C.int(P), C.int(S), C.int(L), C.int(R), C.int(E),
         C.int(H), C.int(NB), C.int(MB))
 }
-
-func MultUnAlignedTransA(C, A, B []float64, alpha, beta float64, ldC, ldA, ldB, P, S, L, R, E, H, NB, MB int) {
-    var Cm C.mdata_t;
-    var Am C.mdata_t;
-    var Bm C.mdata_t;
-    Cm.md =  (*C.double)(unsafe.Pointer(&C[0]))
-    Cm.step = C.int(ldC)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-
-    C.mult_mdata_unaligned_transa(
-        (*C.mdata_t)(unsafe.Pointer(&Cm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        C.double(alpha), C.double(beta),
-        C.int(P), C.int(S), C.int(L), C.int(R), C.int(E),
-        C.int(H), C.int(NB), C.int(MB))
-}
-
 
 func MultAlignedTransA(C, A, B []float64, alpha, beta float64, ldC, ldA, ldB, P, S, L, R, E, H, NB, MB int) {
     var Cm C.mdata_t;
@@ -88,7 +67,7 @@ func MultAlignedTransA(C, A, B []float64, alpha, beta float64, ldC, ldA, ldB, P,
     Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
     Bm.step = C.int(ldB)
 
-    C.mult_mdata_aligned_transa(
+    C.dmult_aligned_transa(
         (*C.mdata_t)(unsafe.Pointer(&Cm)),
         (*C.mdata_t)(unsafe.Pointer(&Am)),
         (*C.mdata_t)(unsafe.Pointer(&Bm)),
@@ -96,6 +75,70 @@ func MultAlignedTransA(C, A, B []float64, alpha, beta float64, ldC, ldA, ldB, P,
         C.int(P), C.int(S), C.int(L), C.int(R), C.int(E),
         C.int(H), C.int(NB), C.int(MB))
 }
+
+
+func MultUnAlignedTransA(C, A, B []float64, alpha, beta float64, ldC, ldA, ldB, P, S, L, R, E, H, NB, MB int) {
+    var Cm C.mdata_t;
+    var Am C.mdata_t;
+    var Bm C.mdata_t;
+    Cm.md =  (*C.double)(unsafe.Pointer(&C[0]))
+    Cm.step = C.int(ldC)
+    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
+    Am.step = C.int(ldA)
+    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
+    Bm.step = C.int(ldB)
+
+    C.dmult_unaligned_transa(
+        (*C.mdata_t)(unsafe.Pointer(&Cm)),
+        (*C.mdata_t)(unsafe.Pointer(&Am)),
+        (*C.mdata_t)(unsafe.Pointer(&Bm)),
+        C.double(alpha), C.double(beta),
+        C.int(P), C.int(S), C.int(L), C.int(R), C.int(E),
+        C.int(H), C.int(NB), C.int(MB))
+}
+
+
+func MultAlignedTransB(C, A, B []float64, alpha, beta float64, ldC, ldA, ldB, P, S, L, R, E, H, NB, MB int) {
+    var Cm C.mdata_t;
+    var Am C.mdata_t;
+    var Bm C.mdata_t;
+    Cm.md =  (*C.double)(unsafe.Pointer(&C[0]))
+    Cm.step = C.int(ldC)
+    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
+    Am.step = C.int(ldA)
+    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
+    Bm.step = C.int(ldB)
+
+    C.dmult_aligned_transb(
+        (*C.mdata_t)(unsafe.Pointer(&Cm)),
+        (*C.mdata_t)(unsafe.Pointer(&Am)),
+        (*C.mdata_t)(unsafe.Pointer(&Bm)),
+        C.double(alpha), C.double(beta),
+        C.int(P), C.int(S), C.int(L), C.int(R), C.int(E),
+        C.int(H), C.int(NB), C.int(MB))
+}
+
+
+func MultUnAlignedTransB(C, A, B []float64, alpha, beta float64, ldC, ldA, ldB, P, S, L, R, E, H, NB, MB int) {
+    var Cm C.mdata_t;
+    var Am C.mdata_t;
+    var Bm C.mdata_t;
+    Cm.md =  (*C.double)(unsafe.Pointer(&C[0]))
+    Cm.step = C.int(ldC)
+    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
+    Am.step = C.int(ldA)
+    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
+    Bm.step = C.int(ldB)
+
+    C.dmult_unaligned_transb(
+        (*C.mdata_t)(unsafe.Pointer(&Cm)),
+        (*C.mdata_t)(unsafe.Pointer(&Am)),
+        (*C.mdata_t)(unsafe.Pointer(&Bm)),
+        C.double(alpha), C.double(beta),
+        C.int(P), C.int(S), C.int(L), C.int(R), C.int(E),
+        C.int(H), C.int(NB), C.int(MB))
+}
+
 
 
 
