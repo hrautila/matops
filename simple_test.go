1f52f3f9f2d5fde7da4f2b94f4e84e3f25b29dff
Harri Rautila <harri.rautila@gmail.com>
2013-01-26 23:32:46 +0200

    Simple test.

---
+++ simple_test.go
@@ -0,0 +1,19 @@
+
+// Copyright (c) Harri Rautila, 2012,2013
+
+// This file is part of github.com/hrautila/matops package. It is free software,
+// distributed under the terms of GNU Lesser General Public License Version 3, or
+// any later version. See the COPYING tile included in this archive.
+
+package matops
+
+import "testing"
+
+func TestCompile(t *testing.T) {
+	t.Logf("Compile OK\n")
+}
+
+// Local Variables:
+// tab-width: 4
+// indent-tabs-mode: nil
+// End:
