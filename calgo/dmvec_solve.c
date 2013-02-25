71b4660ad6bea2b06b9d27677279e8e7e19473a9
Harri Rautila <harri.rautila@gmail.com>
2013-02-25 18:09:44 +0200

    Rewrote solver without temporary buffers. Added blocked solvers, forward and backward.

--- calgo/dmvec_solve.c
+++ calgo/dmvec_solve.c
@@ -18,7 +18,7 @@
      ---------------      ----   
      a10 | a11 | a12       x1
      ---------------      ----
-     a20 | a12 | A22       x2
+     A20 | a21 | A22       x2
 
    i = n: dimensions,
        a11 = 1*1, a10 = 1*n, a01 = n*1, a12 = N-(n+1)*1, A00 = n*n
@@ -26,95 +26,88 @@
 
    A02, a12 are zeros, A00, A22 are lower tridiagonal
 
-   if A diagonal is non-UNIT
-      x1 = (x1 - a10*x0) / a11
-   if a diagonal is UNIT
-      x1 = x1 - a10*x0
+   Variant 0:
+      if A diagonal is non-UNIT
+         x1 = (x1 - a10*x0) / a11
+      else
+         x1 = x1 - a10*x0
+   
+   Variant 1:
+      if A diagonal is non-UNIT
+         x1 = x1/a11
+         x2 = x2 - a21*x1;
+      else
+         x1 = x1
+         x2 = x2 - a21*x1
 
-   Operation a10*x0 is a DOT operation, and results 2n memory reads and 1 write.
-   Vector a10 is row vector with elements N elements a part and x0 is column vector.
+   Variant 0 involves operation a10*x0 that is a DOT operation, and results 2n memory reads
+   and 1 write. Vector a10 is row vector with elements N elements a part and x0 is column vector.
    Accessing x[i+1] is moving in memory direction and is likely a cache-hit. Accesing
    a10[i+1] is a memory reference to N*sizeof(double) bytes far and a likely cache-miss.
    
-   A possible performace optimization: vector Y with size of X, initially zero.
-   Y: 
-      y0
-     ----
-      y1
-     ----
-      y2
+   Variant 1 update x2 with AXPY operation. Vector a21 is column vector and access to
+   a21[i+1] is likely a cache-hit. A drawback is that we have in addition to read x2
+   also write x2 which all in all causes N*N/2 read and write operations to x.
 
-   when i == n: y0 is n*1, y1 is 1*1 and y2 is N-(n+1)*1
-   and y1 == a10*x0,
-
-   x1 = (x1 - y1) / a11 or x1 = x1 - y1 if A is UNIT diagonal
-   y2 = y2 + x1*a12
-   
-   a12 is column vector and a12[i+1] is likely to be cache-hit. A drawback is that
-   we need write y memory locations N*N/2 times. Accessing vector x generates n memory
-   reads and writes. 
  */
 
 // solves backward a diagonal block and updates Xc values.  Yc vector
 // contains per row accumulated values for already solved X's. 
 static void
-_dmvec_solve_backward(double *Xc, const double *Ac, double *Yc, int unit, 
-                      int incX, int ldA, int incY, int nRE)
+_dmvec_solve_backward(double *Xc, const double *Ac, int unit, 
+                      int incX, int ldA, int nRE)
 {
   // Y is 
   register int i;
-  double *xr, *yr, xtmp;
-  const double *Ar, *Acl;
+  double *x0, *x1;
+  const double *a11, *A01;
 
   // upper diagonal matrix of nRE rows/cols and vector X, Y of length nRE
   // move to point to last column and last entry of X.
-  Acl = Ac + (nRE-1)*ldA;
-  Ar = Acl;
-  xr = Xc + (nRE-1)*incX;
-  yr = Yc + (nRE-1)*incY;
+  A01 = Ac + (nRE-1)*ldA;
+  x1 = Xc + (nRE-1)*incX;
+  x0 = Xc;
 
   // xr is the current X element, Ar is row in current A column.
   for (i = nRE-1; i >= 0; i--) {
-    Ar = Acl + i;                // move on diagonal
-    xtmp = xr[0] - yr[0];
-    xr[0] = unit ? xtmp : xtmp/Ar[0];
+    a11 = A01 + i;                // move on diagonal
+    x1[0] = unit ? x1[0] : x1[0]/a11[0];
 
-    // update all y-values with in current column (i is the count above current row)
-    _inner_vec_daxpy(Yc, incY, Acl, xr, incX, 1.0, i);
-    // previous X, previous Y,  previous column in A 
-    xr  -= incX;
-    yr  -= incY;
-    Acl -= ldA;
+    // update all x0-values with in current column (i is the count above current row)
+    _inner_vec_daxpy(x0, incX, A01, x1, incX, -1.0, i);
+    // repartition: previous X, previous column in A 
+    x1  -= incX;
+    A01 -= ldA;
   }
 }
 
 // solves forward a diagonal block and updates Xc values.  Yc vector
 // contains per row accumulated values for already solved X's. 
 static void
-_dmvec_solve_forward(double *Xc, const double *Ac, double *Yc, int unit,
-                     int incX, int ldA, int incY, int nRE)
+_dmvec_solve_forward(double *Xc, const double *Ac, int unit,
+                     int incX, int ldA, int nRE)
 {
   // Y is 
   register int i;
-  double *xr, *yr, xtmp;
-  const double *Ar;
+  register double *x1, *x2, xtmp;
+  const double *a11, *a21;
 
   // lower diagonal matrix of nRE rows/cols and vector X, Y of length nRE
-  Ar = Ac;
-  xr = Xc;
-  yr = Yc;
+  a11 = Ac;
+  x1 = Xc;
+  x2 = x1 + incX;
 
   // xr is the current X element, Ar is row in current A column.
   for (i = 0; i < nRE; i++) {
-    Ar = Ac + i;                // move on diagonal
-    xtmp = xr[0] - yr[0];
-    xr[0] = unit ? xtmp : xtmp/Ar[0];
-    // update all y-values with in current column
-    yr += incY;
-    Ar ++;
-    _inner_vec_daxpy(yr, incY, Ar, xr, incX, 1.0, nRE-i-1);
+    a11 = Ac + i;                // move on diagonal
+    a21 = a11 + 1;
+    //xtmp = xr[0] - yr[0];
+    x1[0] = unit ? x1[0] : x1[0]/a11[0];
+    // update all x2-values with in current column
+    _inner_vec_daxpy(x2, incX, a21, x1, incX, -1.0, nRE-1-i);
     // next X, next column in A 
-    xr += incX;
+    x1 += incX;
+    x2 = x1 + incX;
     Ac += ldA;
   }
 }
@@ -127,28 +120,12 @@ extern void memset(void *, int, size_t);
 void dmvec_solve_unb(mvec_t *X, const mdata_t *A, int flags, int N)
 {
   int i, nI;
-  mvec_t Y;
-  double cB[MAX_VEC_NB] __attribute__((aligned(64)));
-  double *ybuf = (double *)0;
   int unit = flags & MTX_UNIT ? 1 : 0;
-  Y.md = cB;
-  Y.inc = 1;
 
-  if (N > MAX_VEC_NB) {
-    Y.md = (double *)calloc(N, sizeof(double));
-    if (! Y.md) {
-      return;
-    }
-  }
-
-  memset(Y.md, 0, N*sizeof(double));
   if (flags & MTX_LOWER) {
-    _dmvec_solve_forward(X->md, A->md, Y.md, unit, X->inc, A->step, 1, N);
+    _dmvec_solve_forward(X->md, A->md, unit, X->inc, A->step, N);
   } else {
-    _dmvec_solve_backward(X->md, A->md, Y.md, unit, X->inc, A->step, 1, N);
-  }
-  if (N > MAX_VEC_NB) {
-    free(Y.md);
+    _dmvec_solve_backward(X->md, A->md, unit, X->inc, A->step, N);
   }
 }
 
@@ -158,7 +135,7 @@ void dmvec_solve_unb(mvec_t *X, const mdata_t *A, int flags, int N)
      ---------------      ----  ----
      A10 | A11 | A12       X1    Y1
      ---------------      ----  ----
-     A20 | A12 | A22       X2    Y2
+     A20 | A21 | A22       X2    Y2
 
    i = k: dimensions,
        A11 = n*n, A10 = n*k, A01 = k*n, A12 = N-(n+k)*n, A00 = k*k
@@ -168,40 +145,67 @@ void dmvec_solve_unb(mvec_t *X, const mdata_t *A, int flags, int N)
    A02, A12 are zeros, A00, A22 are lower tridiagonal
 
 
-   Y0 = Y0 + A10*X0
-   solve_forward_unb(X1, A11, Y0)
+   Lower Variant 0:
+        X1 = X1 - A10*X0
+        solve_forward_unb(X1, A11)
+
+   Lower Variant 2:
+        solve_forward_unb(X1, A11)
+        X2 = X2 - A21 * X1
 
 */
 void dmvec_solve_blocked(mvec_t *X, const mdata_t *A, int flags, int N, int NB)
 {
   int i, nI, nR;
-  mvec_t Y0;
-  mdata_t A10;
-  double cB[MAX_VEC_NB] __attribute__((aligned(16)));
+  mdata_t A01, A11, A21;
+  mvec_t X0, X1, X2;
   int unit = flags & MTX_UNIT ? 1 : 0;
-  Y0.md = cB;
-  Y0.inc = 1;
 
   if (NB <= 0) {
     NB = 68;
   }
 
-  memset(Y0.md, 0, sizeof(cB));
+  A01.step = A->step;
+  A11.step = A->step;
+  A21.step = A->step;
+  X0.inc = X->inc;
+  X1.inc = X->inc;
+  X2.inc = X->inc;
+  X0.md = X->md;
+
   if (flags & MTX_LOWER) {
     nR = 0;
     for (i = 0; i < N; i += NB) {
       nI = N - i < NB ? N - i : NB;
-      if (i > 0) {
-        // calculate Y0 = Y0 + A10*X0
-        A10.md = &A->md[nR];
-        A10.step = A->step;
-        //printf("A10:\n"); print_tile(A10.md, A10.step, nI, nR);
-        dmult_gemv_blocked(&Y0, &A10, X, 1.0, 1.0, 0, 0, nR, 0, nI, 0, 0);
-        //printf("Y0:\n"); print_tile(Y0.md, 1, nR, nI);
-      }
-      // solve forward using Y values 
-      _dmvec_solve_forward(&X->md[i], &A->md[i*A->step+i], Y0.md, unit, X->inc, A->step, 1, nI);
+
+      // Solve forward curent block
+      X1.md = &X->md[i*X->inc];
+      A11.md = &A->md[i*A->step+i];
+      _dmvec_solve_forward(X1.md, A11.md, unit, X->inc, A->step, nI);
       nR += nI;
+
+      // update X2 with new solutions.
+      A21.md = &A->md[i*A->step+nR];
+      X2.md = &X->md[nR*X->inc];
+      //printf("A21:\n"); print_tile(A21.md, A21.step, N-nR, nI);
+      dmult_gemv_blocked(&X2, &A21, &X1, -1.0, 1.0, 0, 0, nI, 0, N-nR, 0, 0);
+    }
+  } else {
+    nR = N;
+    for (i = N-NB; i > 0; i -= NB) {
+      nI = i < 0 ? i+NB : NB;
+      i  = i < 0 ? 0 : i;
+
+      // Solve forward curent block
+      X1.md = &X->md[i*X->inc];
+      A11.md = &A->md[i*A->step+i];
+      _dmvec_solve_backward(X1.md, A11.md, unit, X->inc, A->step, nI);
+      nR -= nI;
+
+      // update X2 with new solutions.
+      A01.md = &A->md[i*A->step];
+      //printf("A01:\n"); print_tile(A01.md, A21.step, nR, nI);
+      dmult_gemv_blocked(&X0, &A01, &X1, -1.0, 1.0, 0, 0, nI, 0, nR, 0, 0);
     }
   }
 }
