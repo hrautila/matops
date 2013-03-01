ed128e41279dacc6cadc23ae7e4284affbfe4970
Harri Rautila <harri.rautila@gmail.com>
2013-03-02 00:11:30 +0200

    Added coefficient.

--- calgo/dmmat_trid.c
+++ calgo/dmmat_trid.c
@@ -34,7 +34,7 @@
 // Calculates backward a diagonal block and updates Xc values from last to first.
 // Updates are calculated in breadth first manner by with successive AXPY operations.
 static void
-_dmmat_trid_axpy_backward(double *Bc, const double *Ac, int unit,
+_dmmat_trid_axpy_backward(double *Bc, const double *Ac, double alpha, int unit,
                           int ldB, int ldA, int nRE, int nC)
 {
   // Y is 
@@ -53,8 +53,8 @@ _dmmat_trid_axpy_backward(double *Bc, const double *Ac, int unit,
     b0 = Bcl;
     for (j = 0; j < nC; j++) {
       // update all b-values below with the current A column and current X
-      _inner_daxpy(b0+1, Ar+1, b0, 1.0, nRE-i);
-      b0[0] = unit ? b0[0] : b0[0]*Ar[0];
+      _inner_daxpy(b0+1, Ar+1, b0, alpha, nRE-i);
+      b0[0] = alpha * (unit ? b0[0] : b0[0]*Ar[0]);
       b0 += ldB;
     }
     // previous row in B, previous column in A 
@@ -66,7 +66,7 @@ _dmmat_trid_axpy_backward(double *Bc, const double *Ac, int unit,
 // Calculates backward a diagonal block and updates Xc values from last to first.
 // Updates are calculated in depth first manner with DOT operations.
 static void
-_dmmat_trid_dot_backward(double *Bc, const double *Ac, int unit,
+_dmmat_trid_dot_backward(double *Bc, const double *Ac, double alpha, int unit,
                          int ldB, int ldA, int nRE, int nC)
 {
   // Y is 
@@ -86,8 +86,8 @@ _dmmat_trid_dot_backward(double *Bc, const double *Ac, int unit,
     b0 = Bc;
     for (j = 0; j < nC; j++) {
       // update all x-values below with the current A column and current X
-      xtmp = unit ? b1[0] : 0.0;
-      _inner_ddot(&xtmp, Acl, b0, 1.0, nRE-unit-i);
+      xtmp = unit ? alpha*b1[0] : 0.0;
+      _inner_ddot(&xtmp, Acl, b0, alpha, nRE-unit-i);
       b1[0] = xtmp;
       b0 += ldB;
       b1 += ldB;
@@ -101,7 +101,7 @@ _dmmat_trid_dot_backward(double *Bc, const double *Ac, int unit,
 
 // Calculate forward a diagonal block and updates Xc values from first to last.
 static void
-_dmmat_trid_axpy_forward(double *Bc, const double *Ac, double unit,
+_dmmat_trid_axpy_forward(double *Bc, const double *Ac, double alpha, int unit,
                          int ldB, int ldA, int nRE, int nC)
 {
   // Y is 
@@ -115,11 +115,11 @@ _dmmat_trid_axpy_forward(double *Bc, const double *Ac, double unit,
   for (i = 0; i < nRE; i++) {
     b0 = Br;
     Bc = Broot;
-    // update all previous x-values with current A column and current X
+    // update all previous B-values with current A column and current B
     for (j = 0; j < nC; j++) {
-      _inner_daxpy(Bc, Ac, b0, 1.0, i);
+      _inner_daxpy(Bc, Ac, b0, alpha, i);
       Ar = Ac + i;
-      b0[0] = unit ? b0[0] : b0[0]*Ar[0];
+      b0[0] = unit ? b0[0] : alpha*b0[0]*Ar[0];
       b0 += ldB;
       Bc += ldB;
     }
@@ -130,9 +130,9 @@ _dmmat_trid_axpy_forward(double *Bc, const double *Ac, double unit,
   }
 }
 
-// Calculate forward a diagonal block and updates Xc values from first to last.
+// Calculate forward a diagonal block and current B-values from first to last.
 static void
-_dmmat_trid_dot_forward(double *Bc, const double *Ac, int unit,
+_dmmat_trid_dot_forward(double *Bc, const double *Ac, double alpha, int unit,
                         int ldB, int ldA, int nRE, int nC)
 {
   // Y is 
@@ -145,11 +145,11 @@ _dmmat_trid_dot_forward(double *Bc, const double *Ac, int unit,
     Ar = Ac + i + unit;
     b0 = Bcr;
     //b1 = Bcr;
-    // update all previous B-values with current A column and current b
+    // update all current B-value with current A column and following b
     for (j = 0; j < nC; j++) {
       //printf("i=%d, j=%d\n", i, j);
-      xtmp = unit ? b0[0] : 0.0;
-      _inner_ddot(&xtmp, Ar, b0, 1.0, nRE-unit-i);
+      xtmp = unit ? alpha*b0[0] : 0.0;
+      _inner_ddot(&xtmp, Ar, b0, alpha, nRE-unit-i);
       b0[0] = xtmp;
       b0 += ldB;
       //b1 += ldB;
@@ -163,7 +163,7 @@ _dmmat_trid_dot_forward(double *Bc, const double *Ac, int unit,
 //extern void memset(void *, int, size_t);
 
 // X = A*X; unblocked version
-void dmmat_trid_unb(mdata_t *B, const mdata_t *A, int flags, int N, int S, int L)
+void dmmat_trid_unb(mdata_t *B, const mdata_t *A, double alpha, int flags, int N, int S, int L)
 {
   // indicates if diagonal entry is unit (=1.0) or non-unit.
   int unit = flags & MTX_UNIT ? 1 : 0;
@@ -171,20 +171,52 @@ void dmmat_trid_unb(mdata_t *B, const mdata_t *A, int flags, int N, int S, int L
   
   if (flags & MTX_UPPER) {
     if (flags & MTX_TRANSA) {
-      _dmmat_trid_dot_backward(Bc, A->md, unit, B->step, A->step, N, L-S);
+      _dmmat_trid_dot_backward(Bc, A->md, alpha, unit, B->step, A->step, N, L-S);
     } else {
-      _dmmat_trid_axpy_forward(Bc, A->md, unit, B->step, A->step, N, L-S);
+      _dmmat_trid_axpy_forward(Bc, A->md, alpha, unit, B->step, A->step, N, L-S);
     }
   } else {
     if (flags & MTX_TRANSA) {
-      _dmmat_trid_dot_forward(Bc, A->md, unit, B->step, A->step, N, L-S);
+      _dmmat_trid_dot_forward(Bc, A->md, alpha, unit, B->step, A->step, N, L-S);
     } else {
-      _dmmat_trid_axpy_backward(Bc, A->md, unit, B->step, A->step, N, L-S);
+      _dmmat_trid_axpy_backward(Bc, A->md, alpha, unit, B->step, A->step, N, L-S);
     }
   }
 }
 
+/*
 
+  A00 | A01 | A02  B0
+  ---------------  --
+   0  | A11 | A12  B1
+  ---------------  --
+   0  |  0  | A22  B2
+
+  B0 = A00*B0 + A01*B1 + A02*B2
+  B1 =          A11*B1 + A12*B2
+  B2 =                   A22*B2
+
+  B0 = trmm_unb(A00,B0) + [A01; A02] * [B1; B2].T
+  B1 = trmm_unb(A11,B1) + A12*B2
+  B2 = trmm_unb(A22,B2)
+ */
+void dmmat_trid_blk(mdata_t *B, const mdata_t *A, double alpha, int flags,
+                    int N, int S, int L, int NB)
+{
+  // S < L <= N
+  int i, nI, nSL;
+  mdata_t A0, A1, B0, B1;
+
+  nSL = L - S;
+  A0.md = &A->md[S*A->step+S];
+  A0.step = A->step;
+  B0.md = &B->md[S*B->step];
+  B0.step = B->step;
+
+  for (i = 0; i < nSL; i += NB) {
+    nI = i < nSL ? NB : nSL - i;
+  }
+}
 
 // Local Variables:
 // indent-tabs-mode: nil
