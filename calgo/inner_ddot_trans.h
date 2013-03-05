0c2b4cc298b605faf0550221457826408c139e68
Harri Rautila <harri.rautila@gmail.com>
2013-03-06 00:06:22 +0200

    inlined AXPY trans version.

--- calgo/inner_ddot_trans.h
+++ calgo/inner_ddot_trans.h
@@ -60,6 +60,56 @@ void _inner_ddot_trans(double *Cr, const double *Ar, const double *Br,
   Cr[0] += f0;
 }
 
+
+static inline void
+_inner_axpy_trans_nr(double *b0, const double *Ar, const double *Br, double alpha, int nC, int ldB)
+{
+  register int i;
+  register double f0;
+  f0 = alpha * Br[0];
+  for (i = 0; i < nC; i++) {
+    b0[0] += f0 * Ar[0];
+    b0 += ldB;
+    Ar++;
+  }
+}
+
+static inline void
+_inner_axpy_trans(double *b0, const double *Ar, const double *Br, double alpha, int nC, int ldB)
+{
+  register int k, iB;
+  register double f0;
+
+  f0 = alpha * Br[0];
+  for (k = 0; k < nC-3; k += 4) {
+    iB = 0;
+    b0[iB] += f0 * Ar[0];
+    iB += ldB;
+    b0[iB] += f0 * Ar[1];
+    iB += ldB;
+    b0[iB] += f0 * Ar[2];
+    iB += ldB;
+    b0[iB] += f0 * Ar[3];
+    Ar += 4;
+    b0 += (ldB << 2);
+  }
+  if (k == nC)
+    return;
+  if (k < nC-1) {
+    iB = 0;
+    b0[iB] += f0 * Ar[0];
+    iB += ldB;
+    b0[iB] += f0 * Ar[1];
+    Ar += 2;
+    b0 += (ldB << 1);
+    k += 2;
+  }
+  if (k < nC) {
+    b0[0] += f0 * Ar[0];
+  }
+}
+
+
 #endif
 
 // Local Variables:
