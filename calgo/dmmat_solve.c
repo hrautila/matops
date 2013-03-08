2d1f1f939198cafcfa6f1290af62d84507c30643
Harri Rautila <harri.rautila@gmail.com>
2013-03-08 23:50:07 +0200

    Mostly working blocked TRSM.

--- calgo/dmmat_solve.c
+++ calgo/dmmat_solve.c
@@ -415,6 +415,228 @@ void dmmat_solve_unb(mdata_t *B, const mdata_t *A, double alpha, int flags, int 
   }
 }
 
+/*  UPPER, LEFT
+
+    A00 | A01 | A02   B0
+   ----------------   --
+     0  | A11 | A12   B1
+   ----------------   --
+     0  |  0  | A22   B2
+
+    B0 = A00*B'0 + A01*B'1 + A02*B'2 --> B'0 = A00.-1*(B0 - A01*B'1 - A02*B'2)
+    B1 = A11*B'1 + A12*B'2           --> B'1 = A11.-1*(B1           - A12*B'2)
+    B2 = A22*B'2                     --> B'2 = A22.-1*B2
+ */
+static void
+_dmmat_solve_blk_upper(mdata_t *B, const mdata_t *A, double alpha, int flags,
+                       int N, int S, int E, int NB, cbuf_t *Acpy, cbuf_t *Bcpy)
+{
+  register int i, j, nI, nJ, cI, cJ;
+  mdata_t A0, A1, B0, B1;
+  A0.step = A->step;
+  A1.step = A->step;
+  B0.step = B->step;
+  B1.step = B->step;
+
+  for (i = N; i > 0; i -= NB) {
+    nI = i < NB ? i : NB;
+    cI = i < NB ? 0 : i-NB;
+    for (j = E; j > S; j -= NB) {
+      nJ = j < S + NB ? S + NB - j : NB;
+      cJ = j < S + NB ? S : j-NB;
+
+      A0.md = &A->md[cI*A->step];           // above the diagonal A block
+      A1.md = &A->md[cI*A->step + cI];      // diagonal A block
+      B0.md = &B->md[cJ*B->step ];          // top B block
+      B1.md = &B->md[cJ*B->step + cI];      // bottom B block
+
+      // solve bottom block
+      dmmat_solve_unb(&B1, &A1, alpha, flags, nI, 0, nJ);
+      // update top with bottom solution
+      _dblock_mult_panel(&B0, &A0, &B1, -1.0, 0, nI, nJ, i-nI, NB, Acpy, Bcpy);
+    }
+  }
+}
+
+/*  UPPER, TRANS, LEFT
+
+    A00 | A01 | A02   B0
+   ----------------   --
+     0  | A11 | A12   B1
+   ----------------   --
+     0  |  0  | A22   B2
+
+    B0 = A00*B'0                     --> B'0 = A00.-1*B0
+    B1 = A01*B'0 + A11*B'1           --> B'1 = A11.-1*(B1 - A01*B'0)
+    B2 = A02*B'0 + A12*B'1 + A22*B'2 --> B'2 = A22.-1*(B2 - A02*B'0 - A12*B'1)
+ */
+static void
+_dmmat_solve_blk_u_trans(mdata_t *B, const mdata_t *A, double alpha, int flags,
+                         int N, int S, int E, int NB, cbuf_t *Acpy, cbuf_t *Bcpy)
+{
+  register int i, j, nI, nJ, cI, cJ;
+  mdata_t A0, A1, B0, B1;
+  A0.step = A->step;
+  A1.step = A->step;
+  B0.step = B->step;
+  B1.step = B->step;
+
+  for (i = 0; i < N; i += NB) {
+    nI = i < N - NB ? NB : N - i;
+    cI = nI < NB ? N-nI : i;
+    for (j = S; j < E; j += NB) {
+      nJ = j < E - NB ? NB : E - j;
+      cJ = nJ < NB ? E - nJ : j;
+
+      A0.md = &A->md[cI*A->step];        // above the diagonal A block
+      A1.md = &A->md[cI*A->step + cI];   // diagonal A block
+      B0.md = &B->md[cJ*B->step];        // top B block
+      B1.md = &B->md[cJ*B->step + cI];   // bottom B block
+
+      printf("i:%d, j:%d, nI:%d, nJ:%d, N-i-nI:%d, cI:%d, cJ:%d\n", i, j, nI, nJ, N-i-nI, cI, cJ);
+      printf("..A0:\n"); print_tile(A0.md, A0.step, cI, nI);
+      printf("..B0:\n"); print_tile(B0.md, B0.step, cI, nJ);
+      printf("..A1:\n"); print_tile(A1.md, A1.step, nI, nI);
+      printf("..B1:\n"); print_tile(B1.md, B1.step, nI, nJ);
+
+      // update bottom block with top block
+      _dblock_mult_panel(&B1, &A0, &B0, -1.0, MTX_TRANSA, nI, nJ, cI, NB, Acpy, Bcpy);
+      printf("=1 B1:\n"); print_tile(B1.md, B1.step, nI, nJ);
+      printf("   B0:\n"); print_tile(B0.md, B0.step, cI, nJ);
+      // solve bottom block
+      dmmat_solve_unb(&B1, &A1, alpha, flags, nI, 0, nJ);
+      printf("=2 B1:\n"); print_tile(B1.md, B1.step, nI, nJ);
+      printf("   B0:\n"); print_tile(B0.md, B0.step, cI, nJ);
+    }
+  }
+}
+
+/*  LOWER, LEFT
+
+    A00 |  0  |  0    B0
+   ----------------   --
+    A10 | A11 |  0    B1
+   ----------------   --
+    A20 | A21 | A22   B2
+
+    B0 = A00*B'0                     --> B'0 = A00.-1*B0
+    B1 = A10*B'0 + A11*B'1           --> B'1 = A11.-1*(B1 - A10*B'0)
+    B2 = A20*B'0 + A21*B'1 + A22*B'2 --> B'2 = A22.-1*(B2 - A20*B'0 - A21*B'1)
+ */
+static void
+_dmmat_solve_blk_lower(mdata_t *B, const mdata_t *A, double alpha, int flags,
+                       int N, int S, int E, int NB, cbuf_t *Acpy, cbuf_t *Bcpy)
+{
+  register int i, j, nI, nJ, cI, cJ;
+  mdata_t A0, A1, B0, B1;
+  A0.step = A->step;
+  A1.step = A->step;
+  B0.step = B->step;
+  B1.step = B->step;
+
+  for (i = 0; i < N; i += NB) {
+    nI = i < N - NB ? NB : N - i;
+    cI = nI < NB ? N-nI : i;
+    for (j = S; j < E; j += NB) {
+      nJ = j < E - NB ? NB : E - j;
+      cJ = nJ < NB ? E - nJ : j;
+
+      A0.md = &A->md[cI*A->step + cI];      // diagonal A block
+      A1.md = &A->md[cI*A->step + cI+nI];   // below the diagonal A block
+      B0.md = &B->md[cJ*B->step + cI];      // top B block
+      B1.md = &B->md[cJ*B->step + cI+nI];   // bottom B block
+
+      // solve top block
+      dmmat_solve_unb(&B0, &A0, alpha, flags, nI, 0, nJ);
+      // update bottom block with top block
+      _dblock_mult_panel(&B1, &A1, &B0, -1.0, 0, nI, nJ, N-i-nI, NB, Acpy, Bcpy);
+    }
+  }
+}
+
+/*  LOWER, TRANSA, LEFT
+
+    A00 |  0  |  0    B0
+   ----------------   --
+    A10 | A11 |  0    B1
+   ----------------   --
+    A20 | A21 | A22   B2
+
+    B0 = A00*B'0 + A10*B'1 + A20*B'2 --> B'0 = A00.-1*(B0 - A10*B'1 - A20*B'2)
+    B1 = A11*B'1 + A21*B'2           --> B'1 = A11.-1*(B1           - A21*B'2)
+    B2 = A22*B'2                     --> B'2 = A22.-1*B2
+ */
+static void
+_dmmat_solve_blk_l_trans(mdata_t *B, const mdata_t *A, double alpha, int flags,
+                         int N, int S, int E, int NB, cbuf_t *Acpy, cbuf_t *Bcpy)
+{
+  register int i, j, nI, nJ, cI, cJ;
+  mdata_t A0, A1, B0, B1;
+  A0.step = A->step;
+  A1.step = A->step;
+  B0.step = B->step;
+  B1.step = B->step;
+
+  for (i = N; i > 0; i -= NB) {
+    nI = i < NB ? i : NB;
+    cI = i < NB ? 0 : i-NB;
+    for (j = E; j > S; j -= NB) {
+      nJ = j < S + NB ? S + NB - j : NB;
+      cJ = j < S + NB ? S : j-NB;
+
+      A0.md = &A->md[cI*A->step + cI];      // diagonal A block
+      A1.md = &A->md[cI*A->step + i];        // below the diagonal A block
+      B0.md = &B->md[cJ*B->step + cI];      // top B block
+      B1.md = &B->md[cJ*B->step + i];       // bottom B block
+
+      //printf("i:%d, j:%d, nI:%d, nJ:%d, N-i:%d, cI:%d, cJ:%d\n", i, j, nI, nJ, N-i, cI, cJ);
+      //printf("..A0:\n"); print_tile(A0.md, A0.step, nI, nI);
+      //printf("..B0:\n"); print_tile(B0.md, B0.step, nI, nJ);
+      //printf("..A1:\n"); print_tile(A1.md, A1.step, N-i, nI);
+      //printf("..B1:\n"); print_tile(B1.md, B1.step, N-i, nJ);
+
+      // update top with bottom solution
+      _dblock_mult_panel(&B0, &A1, &B1, -1.0, MTX_TRANSA, N-i, nJ, nI, NB, Acpy, Bcpy);
+      // solve top block
+      dmmat_solve_unb(&B0, &A0, alpha, flags, nI, 0, nJ);
+    }
+  }
+}
+
+// B = A.-1*B; unblocked
+void dmmat_solve_blk(mdata_t *B, const mdata_t *A, double alpha, int flags,
+                     int N, int S, int E, int NB)
+{
+  // S < E <= N
+  double Abuf[MAX_VP_ROWS*MAX_VP_COLS] __attribute__((aligned(16)));
+  double Bbuf[MAX_VP_ROWS*MAX_VP_COLS] __attribute__((aligned(16)));
+  cbuf_t Acpy = {Abuf, MAX_VP_COLS*MAX_VP_COLS};
+  cbuf_t Bcpy = {Bbuf, MAX_VP_COLS*MAX_VP_COLS};
+
+  if (E-S <= 0)
+    return;
+
+  if (NB > MAX_VP_COLS || NB <= 0) {
+    NB = MAX_VP_COLS;
+  }
+  if (flags & MTX_RIGHT) {
+  } else {
+    // B = A.-1*B; B = A.-T*B
+    if (flags & MTX_UPPER) {
+      if (flags & MTX_TRANSA) {
+        _dmmat_solve_blk_u_trans(B, A, alpha, flags, N, S, E, NB, &Acpy, &Bcpy);
+      } else {
+        _dmmat_solve_blk_upper(B, A, alpha, flags, N, S, E, NB, &Acpy, &Bcpy);
+      }
+    } else {
+      if (flags & MTX_TRANSA) {
+        _dmmat_solve_blk_l_trans(B, A, alpha, flags, N, S, E, NB, &Acpy, &Bcpy);
+      } else {
+        _dmmat_solve_blk_lower(B, A, alpha, flags, N, S, E, NB, &Acpy, &Bcpy);
+      }
+    }
+  }
+}
 // Local Variables:
 // indent-tabs-mode: nil
 // End:
