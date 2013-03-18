9b6142ba87605c55e770ad72ad327efc1620760a
Harri Rautila <harri.rautila@gmail.com>
2013-03-19 00:26:38 +0200

    Removed -funroll-loops to have tighter code.

--- calgo/cmops.go
+++ calgo/cmops.go
@@ -8,23 +8,28 @@
 
 package calgo
 
-// #cgo CFLAGS: -O3 -msse4.1 -funroll-loops -fomit-frame-pointer -ffast-math 
+// -O3 -msse4.1 -funroll-loops -fomit-frame-pointer -ffast-math 
+
+// #cgo CFLAGS: -O3 -msse4.1 -fomit-frame-pointer -ffast-math 
 // #include "cmops.h"
 import "C"
 import "unsafe"
 
 type Flags int
 const (
-    TRANSA = (1 << iota)        // 0x1
-    TRANSB                      // 0x2
-    LOWER                       // 0x4
-    UPPER                       // 0x8
-    LEFT                        // 0x10
-    RIGHT                       // 0x20
-    UNIT                        // 0x40
+    TRANSA = (1 << iota)        // 0x1  ; transpose A
+    TRANSB                      // 0x2  ; transpose B
+    LOWER                       // 0x4  ; lower tridiagonal
+    UPPER                       // 0x8  ; upper tridiagonal
+    LEFT                        // 0x10 ; A on left side
+    RIGHT                       // 0x20 ; A on right side
+    UNIT                        // 0x40 ; unit diagonal
+    TRANS                       // 0x80 ; generic transpose
     NOTRANS = 0
     NULL = 0
 )
+
+// Matrix-Matrix operators
 
 // Generic matrix-matrix multiplication for block [R:E, S:L] with panel length P.
 //
@@ -50,28 +55,6 @@ func DMult(C, A, B []float64, alpha, beta float64, trans Flags, ldC, ldA, ldB, P
     Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
     Bm.step = C.int(ldB)
 
-    C.dmult_mm_blocked(
-        (*C.mdata_t)(unsafe.Pointer(&Cm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        C.double(alpha), C.double(beta), C.int(trans),
-        C.int(P), C.int(S), C.int(L), C.int(R), C.int(E),
-        C.int(H), C.int(NB), C.int(MB))
-}
-
-func DMult2(C, A, B []float64, alpha, beta float64, trans Flags, ldC, ldA, ldB, P, S, L, R, E, H, NB, MB int) {
-
-    var Cm C.mdata_t
-    var Am C.mdata_t
-    var Bm C.mdata_t
-
-    Cm.md =  (*C.double)(unsafe.Pointer(&C[0]))
-    Cm.step = C.int(ldC)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-
     C.dmult_mm_blocked2(
         (*C.mdata_t)(unsafe.Pointer(&Cm)),
         (*C.mdata_t)(unsafe.Pointer(&Am)),
@@ -82,29 +65,8 @@ func DMult2(C, A, B []float64, alpha, beta float64, trans Flags, ldC, ldA, ldB, 
 }
 
 
+// 
 func DMultSymm(C, A, B []float64, alpha, beta float64, flags Flags, ldC, ldA, ldB, P, S, L, R, E, H, NB, MB int) {
-
-    var Cm C.mdata_t
-    var Am C.mdata_t
-    var Bm C.mdata_t
-
-    Cm.md =  (*C.double)(unsafe.Pointer(&C[0]))
-    Cm.step = C.int(ldC)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-
-    C.dmult_symm_blocked(
-        (*C.mdata_t)(unsafe.Pointer(&Cm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        C.double(alpha), C.double(beta), C.int(flags),
-        C.int(P), C.int(S), C.int(L), C.int(R), C.int(E),
-        C.int(H), C.int(NB), C.int(MB))
-}
-
-func DMultSymm2(C, A, B []float64, alpha, beta float64, flags Flags, ldC, ldA, ldB, P, S, L, R, E, H, NB, MB int) {
 
     var Cm C.mdata_t
     var Am C.mdata_t
@@ -126,7 +88,119 @@ func DMultSymm2(C, A, B []float64, alpha, beta float64, flags Flags, ldC, ldA, l
         C.int(H), C.int(NB), C.int(MB))
 }
 
+// blas TRMM; unblocked
+// S is the start column (LEFT), row (RIGHT); E is the end column (LEFT), row (RIGHT)
+func DTrmmUnblk(B, A []float64, alpha float64, flags Flags, ldB, ldA, N, S, E, NB int) {
+    var Bm C.mdata_t
+    var Am C.mdata_t
+    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
+    Bm.step = C.int(ldB)
+    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
+    Am.step = C.int(ldA)
 
+    C.dmmat_trid_unb(
+        (*C.mdata_t)(unsafe.Pointer(&Bm)),
+        (*C.mdata_t)(unsafe.Pointer(&Am)),
+        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(E),
+        /*C.int(R), C.int(E),*/)
+
+}
+
+// blas TRMM; blocked
+func DTrmmBlk(B, A []float64, alpha float64, flags Flags, ldB, ldA, N, S, E, NB int) {
+    var Bm C.mdata_t
+    var Am C.mdata_t
+    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
+    Bm.step = C.int(ldB)
+    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
+    Am.step = C.int(ldA)
+
+    C.dmmat_trmm_blk(
+        (*C.mdata_t)(unsafe.Pointer(&Bm)),
+        (*C.mdata_t)(unsafe.Pointer(&Am)),
+        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(E),
+        /*C.int(R), C.int(E),*/ C.int(NB))
+
+}
+
+// blas TRSM; unblocked
+// S is the start column (LEFT), row (RIGHT); E is the end column (LEFT), row (RIGHT)
+func DSolveUnblk(B, A []float64, alpha float64, flags Flags, ldB, ldA, N, S, E int) {
+    var Bm C.mdata_t
+    var Am C.mdata_t
+    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
+    Bm.step = C.int(ldB)
+    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
+    Am.step = C.int(ldA)
+
+    C.dmmat_solve_unb(
+        (*C.mdata_t)(unsafe.Pointer(&Bm)),
+        (*C.mdata_t)(unsafe.Pointer(&Am)),
+        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(E))
+
+}
+
+// blas TRSM; blocked
+// S is the start column (LEFT), row (RIGHT); E is the end column (LEFT), row (RIGHT)
+func DSolveBlk(B, A []float64, alpha float64, flags Flags, ldB, ldA, N, S, E, NB int) {
+    var Bm C.mdata_t
+    var Am C.mdata_t
+    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
+    Bm.step = C.int(ldB)
+    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
+    Am.step = C.int(ldA)
+
+    C.dmmat_solve_blk(
+        (*C.mdata_t)(unsafe.Pointer(&Bm)),
+        (*C.mdata_t)(unsafe.Pointer(&Am)),
+        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(E), C.int(NB))
+
+}
+
+// blas SYRK; blocked
+// S is the start column and row in C; E is the end column and row in C
+func DSymmRankBlk(C, A []float64, alpha, beta float64, flags Flags, ldC, ldA, N, S, E, H, NB int) {
+    var Cm C.mdata_t
+    var Am C.mdata_t
+    Cm.md =  (*C.double)(unsafe.Pointer(&C[0]))
+    Cm.step = C.int(ldC)
+    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
+    Am.step = C.int(ldA)
+
+    C.dmmat_rank_blk(
+        (*C.mdata_t)(unsafe.Pointer(&Cm)),
+        (*C.mdata_t)(unsafe.Pointer(&Am)),
+        C.double(alpha), C.double(beta),
+        C.int(flags), C.int(N), C.int(S), C.int(E), C.int(H), C.int(NB))
+
+}
+
+// blas SYR2K; blocked
+// S is the start column and row in C; E is the end column and row in C
+func DSymmRank2Blk(C, A, B []float64, alpha, beta float64, flags Flags, ldC, ldA, ldB, N, S, E, H, NB int) {
+    var Cm C.mdata_t
+    var Am C.mdata_t
+    var Bm C.mdata_t
+    Cm.md =  (*C.double)(unsafe.Pointer(&C[0]))
+    Cm.step = C.int(ldC)
+    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
+    Am.step = C.int(ldA)
+    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
+    Bm.step = C.int(ldB)
+
+    C.dmmat_rank2_blk(
+        (*C.mdata_t)(unsafe.Pointer(&Cm)),
+        (*C.mdata_t)(unsafe.Pointer(&Am)),
+        (*C.mdata_t)(unsafe.Pointer(&Bm)),
+        C.double(alpha), C.double(beta),
+        C.int(flags), C.int(N), C.int(S), C.int(E), C.int(H), C.int(NB))
+
+}
+
+
+// Matrix-Vector operators
+
+// blas GEMV; blocked version
 // Y = alpha*A*X + beta*Y; Y is M*1, X is N*1 and A is M*N
 func DMultMV(Y, A, X []float64, alpha, beta float64, flags Flags, incY, ldA, incX, S, L, R, E, H, MB int) {
     var Yv C.mvec_t;
@@ -148,6 +222,8 @@ func DMultMV(Y, A, X []float64, alpha, beta float64, flags Flags, incY, ldA, inc
         C.int(H), C.int(MB))
 }
 
+
+// blas GER; blocked version
 // A = A + alpha * x * y.T; A is M*N, x is M*1, Y is N*1, 0 < R < E <= M, 0 < S < L <= N
 func DRankMV(A, X, Y []float64, alpha float64, ldA, incX, incY, S, L, R, E, NB, MB int) {
     var Yv C.mvec_t;
@@ -171,6 +247,8 @@ func DRankMV(A, X, Y []float64, alpha float64, ldA, incX, incY, S, L, R, E, NB, 
 
 
 // A = A + alpha * x * y.T; A is M*N, x is M*1, Y is N*1, 0 < R < E <= M, 0 < S < L <= N
+
+// blas SYR; blocked version
 func DSymmRankMV(A, X []float64, alpha float64, flags Flags, ldA, incX, S, L, NB int) {
     var Xv C.mvec_t
     var Am C.mdata_t
@@ -187,6 +265,8 @@ func DSymmRankMV(A, X []float64, alpha float64, flags Flags, ldA, incX, S, L, NB
 }
 
 // A = A + alpha * x * y.T; A is M*N, x is M*1, Y is N*1, 0 < R < E <= M, 0 < S < L <= N
+
+// blas SYR2; blocked version
 func DSymmRank2MV(A, X, Y []float64, alpha float64, flags Flags, ldA, incX, incY, S, L, NB int) {
     var Xv C.mvec_t;
     var Yv C.mvec_t;
@@ -207,7 +287,8 @@ func DSymmRank2MV(A, X, Y []float64, alpha float64, flags Flags, ldA, incX, incY
 }
 
 
-func DSolveLower(X, A []float64, unit bool, incX, ldA, N, NB int) {
+// blas TSMV; unblocked version
+func DSolveUnblkMV(X, A []float64, flags Flags, incX, ldA, N int) {
     var Xv C.mvec_t
     var Am C.mdata_t
     Xv.md =  (*C.double)(unsafe.Pointer(&X[0]))
@@ -215,10 +296,6 @@ func DSolveLower(X, A []float64, unit bool, incX, ldA, N, NB int) {
     Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
     Am.step = C.int(ldA)
 
-    var flags Flags = LOWER
-    if unit {
-        flags |= UNIT
-    }
     C.dmvec_solve_unb(
         (*C.mvec_t)(unsafe.Pointer(&Xv)),
         (*C.mdata_t)(unsafe.Pointer(&Am)),
@@ -226,7 +303,8 @@ func DSolveLower(X, A []float64, unit bool, incX, ldA, N, NB int) {
 
 }
 
-func DSolveLowerBlocked(X, A []float64, unit bool, incX, ldA, N, NB int) {
+// blas TSMV; blocked version
+func DSolveBlkMV(X, A []float64, flags Flags, incX, ldA, N, NB int) {
     var Xv C.mvec_t
     var Am C.mdata_t
     Xv.md =  (*C.double)(unsafe.Pointer(&X[0]))
@@ -234,10 +312,6 @@ func DSolveLowerBlocked(X, A []float64, unit bool, incX, ldA, N, NB int) {
     Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
     Am.step = C.int(ldA)
 
-    var flags Flags = LOWER
-    if unit {
-        flags |= UNIT
-    }
     C.dmvec_solve_blocked(
         (*C.mvec_t)(unsafe.Pointer(&Xv)),
         (*C.mdata_t)(unsafe.Pointer(&Am)),
@@ -245,7 +319,8 @@ func DSolveLowerBlocked(X, A []float64, unit bool, incX, ldA, N, NB int) {
 
 }
 
-func DSolveUpper(X, A []float64, unit bool, incX, ldA, N, NB int) {
+// blas TRMV; unblocked
+func DTrimvUnblkMV(X, A []float64, flags Flags, incX, ldA, N int) {
     var Xv C.mvec_t
     var Am C.mdata_t
     Xv.md =  (*C.double)(unsafe.Pointer(&X[0]))
@@ -253,48 +328,6 @@ func DSolveUpper(X, A []float64, unit bool, incX, ldA, N, NB int) {
     Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
     Am.step = C.int(ldA)
 
-    var flags Flags = UPPER
-    if unit {
-        flags |= UNIT
-    }
-    C.dmvec_solve_unb(
-        (*C.mvec_t)(unsafe.Pointer(&Xv)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.int(flags), C.int(N))
-
-}
-
-func DSolveUpperBlocked(X, A []float64, unit bool, incX, ldA, N, NB int) {
-    var Xv C.mvec_t
-    var Am C.mdata_t
-    Xv.md =  (*C.double)(unsafe.Pointer(&X[0]))
-    Xv.inc = C.int(incX)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    var flags Flags = UPPER
-    if unit {
-        flags |= UNIT
-    }
-    C.dmvec_solve_blocked(
-        (*C.mvec_t)(unsafe.Pointer(&Xv)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.int(flags), C.int(N), C.int(NB))
-
-}
-
-func DTrimvUpper(X, A []float64, unit bool, incX, ldA, N, NB int) {
-    var Xv C.mvec_t
-    var Am C.mdata_t
-    Xv.md =  (*C.double)(unsafe.Pointer(&X[0]))
-    Xv.inc = C.int(incX)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    var flags Flags = UPPER
-    if unit {
-        flags |= UNIT
-    }
     C.dmvec_trid_unb(
         (*C.mvec_t)(unsafe.Pointer(&Xv)),
         (*C.mdata_t)(unsafe.Pointer(&Am)),
@@ -302,241 +335,7 @@ func DTrimvUpper(X, A []float64, unit bool, incX, ldA, N, NB int) {
 
 }
 
-func DTrimvUpperTransA(X, A []float64, unit bool, incX, ldA, N, NB int) {
-    var Xv C.mvec_t
-    var Am C.mdata_t
-    Xv.md =  (*C.double)(unsafe.Pointer(&X[0]))
-    Xv.inc = C.int(incX)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
 
-    var flags Flags = UPPER|TRANSA
-    if unit {
-        flags |= UNIT
-    }
-    C.dmvec_trid_unb(
-        (*C.mvec_t)(unsafe.Pointer(&Xv)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.int(flags), C.int(N))
-
-}
-
-func DTrimvLower(X, A []float64, unit bool, incX, ldA, N, NB int) {
-    var Xv C.mvec_t
-    var Am C.mdata_t
-    Xv.md =  (*C.double)(unsafe.Pointer(&X[0]))
-    Xv.inc = C.int(incX)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    var flags Flags = LOWER
-    if unit {
-        flags |= UNIT
-    }
-    C.dmvec_trid_unb(
-        (*C.mvec_t)(unsafe.Pointer(&Xv)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)), 
-        C.int(flags), C.int(N))
-
-}
-
-func DTrimvLowerTransA(X, A []float64, unit bool, incX, ldA, N, NB int) {
-    var Xv C.mvec_t
-    var Am C.mdata_t
-    Xv.md =  (*C.double)(unsafe.Pointer(&X[0]))
-    Xv.inc = C.int(incX)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    var flags Flags = LOWER|TRANSA
-    if unit {
-        flags |= UNIT
-    }
-    C.dmvec_trid_unb(
-        (*C.mvec_t)(unsafe.Pointer(&Xv)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)), 
-        C.int(flags), C.int(N))
-
-}
-
-func DTrmmUpper(B, A []float64, alpha float64, unit bool, ldB, ldA, N, S, L int) {
-    var Bm C.mdata_t
-    var Am C.mdata_t
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    var flags Flags = UPPER
-    if unit {
-        flags |= UNIT
-    }
-    C.dmmat_trid_unb(
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(L))
-
-}
-
-func DTrmmUpperTransA(B, A []float64, alpha float64, unit bool, ldB, ldA, N, S, L int) {
-    var Bm C.mdata_t
-    var Am C.mdata_t
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    var flags Flags = UPPER|TRANSA
-    if unit {
-        flags |= UNIT
-    }
-    C.dmmat_trid_unb(
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(L))
-
-}
-
-func DTrmmLower(B, A []float64, alpha float64, unit bool, ldB, ldA, N, S, L int) {
-    var Bm C.mdata_t
-    var Am C.mdata_t
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    var flags Flags = LOWER
-    if unit {
-        flags |= UNIT
-    }
-    C.dmmat_trid_unb(
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(L))
-
-}
-
-func DTrmmLowerTransA(B, A []float64, alpha float64, unit bool, ldB, ldA, N, S, L int) {
-    var Bm C.mdata_t
-    var Am C.mdata_t
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    var flags Flags = LOWER|TRANSA
-    if unit {
-        flags |= UNIT
-    }
-    C.dmmat_trid_unb(
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(L))
-
-}
-
-// S is the start column (LEFT), row (RIGHT); E is the end column (LEFT), row (RIGHT)
-func DTrmmUnblk(B, A []float64, alpha float64, flags Flags, ldB, ldA, N, S, E, NB int) {
-    var Bm C.mdata_t
-    var Am C.mdata_t
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    C.dmmat_trid_unb(
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(E),
-        /*C.int(R), C.int(E),*/)
-
-}
-
-func DTrmmBlk(B, A []float64, alpha float64, flags Flags, ldB, ldA, N, S, E, NB int) {
-    var Bm C.mdata_t
-    var Am C.mdata_t
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    C.dmmat_trmm_blk(
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(E),
-        /*C.int(R), C.int(E),*/ C.int(NB))
-
-}
-
-// S is the start column (LEFT), row (RIGHT); E is the end column (LEFT), row (RIGHT)
-func DMSolveUnblk(B, A []float64, alpha float64, flags Flags, ldB, ldA, N, S, E int) {
-    var Bm C.mdata_t
-    var Am C.mdata_t
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    C.dmmat_solve_unb(
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(E))
-
-}
-
-// S is the start column (LEFT), row (RIGHT); E is the end column (LEFT), row (RIGHT)
-func DMSolveBlk(B, A []float64, alpha float64, flags Flags, ldB, ldA, N, S, E, NB int) {
-    var Bm C.mdata_t
-    var Am C.mdata_t
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    C.dmmat_solve_blk(
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.double(alpha), C.int(flags), C.int(N), C.int(S), C.int(E), C.int(NB))
-
-}
-
-// S is the start column and row in C; E is the end column and row in C
-func DMRankBlk(C, A []float64, alpha, beta float64, flags Flags, ldC, ldA, N, S, E, H, NB int) {
-    var Cm C.mdata_t
-    var Am C.mdata_t
-    Cm.md =  (*C.double)(unsafe.Pointer(&C[0]))
-    Cm.step = C.int(ldC)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-
-    C.dmmat_rank_blk(
-        (*C.mdata_t)(unsafe.Pointer(&Cm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        C.double(alpha), C.double(beta),
-        C.int(flags), C.int(N), C.int(S), C.int(E), C.int(H), C.int(NB))
-
-}
-
-// S is the start column and row in C; E is the end column and row in C
-func DMRank2Blk(C, A, B []float64, alpha, beta float64, flags Flags, ldC, ldA, ldB, N, S, E, H, NB int) {
-    var Cm C.mdata_t
-    var Am C.mdata_t
-    var Bm C.mdata_t
-    Cm.md =  (*C.double)(unsafe.Pointer(&C[0]))
-    Cm.step = C.int(ldC)
-    Am.md =  (*C.double)(unsafe.Pointer(&A[0]))
-    Am.step = C.int(ldA)
-    Bm.md =  (*C.double)(unsafe.Pointer(&B[0]))
-    Bm.step = C.int(ldB)
-
-    C.dmmat_rank2_blk(
-        (*C.mdata_t)(unsafe.Pointer(&Cm)),
-        (*C.mdata_t)(unsafe.Pointer(&Am)),
-        (*C.mdata_t)(unsafe.Pointer(&Bm)),
-        C.double(alpha), C.double(beta),
-        C.int(flags), C.int(N), C.int(S), C.int(E), C.int(H), C.int(NB))
-
-}
 
 /*
 func copy_trans(C, A []float64, ldC, ldA, M, N int) {
