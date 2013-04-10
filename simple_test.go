69d88bec7039f8ec6576f5e61584861a43cfde0c
Harri Rautila <harri.rautila@gmail.com>
2013-04-11 01:10:05 +0300

    Matrix view update test.

--- simple_test.go
+++ simple_test.go
@@ -8,9 +8,7 @@
 package matops
 
 import (
-    //"github.com/hrautila/matrix"
-    //"github.com/hrautila/linalg"
-    //"github.com/hrautila/linalg/blas"
+    "github.com/hrautila/matrix"
     "testing"
 )
 
@@ -18,6 +16,36 @@ func TestCompile(t *testing.T) {
 	t.Logf("Compile OK\n")
 }
 
+
+func TestViewUpdate(t *testing.T) {
+    Adata2 := [][]float64{
+        []float64{4.0, 2.0, 2.0},
+        []float64{6.0, 4.0, 2.0},
+        []float64{4.0, 6.0, 1.0},
+    }
+
+    A := matrix.FloatMatrixFromTable(Adata2, matrix.RowOrder)
+    N := A.Rows()
+
+    // simple LU decomposition without pivoting
+    var A11, a10, a01, a00 matrix.FloatMatrix
+    for k := 1; k < N; k++ {
+        a00.SubMatrixOf(A, k-1, k-1, 1, 1)
+        a01.SubMatrixOf(A, k-1, k,   1, A.Cols()-k)
+        a10.SubMatrixOf(A, k,   k-1, A.Rows()-k, 1)
+        A11.SubMatrixOf(A, k,   k)
+        //t.Logf("A11: %v  a01: %v\n", A11, a01)
+        a10.Scale(1.0/a00.Float())
+        MVRankUpdate(&A11, &a10, &a01, -1.0)
+    }
+
+    Ld := TriLU(A.Copy())
+    Ud := TriU(A)
+    t.Logf("Ld:\n%v\nUd:\n%v\n", Ld, Ud)
+    An := matrix.FloatZeros(N, N)
+    Mult(An, Ld, Ud, 1.0, 1.0, NOTRANS)
+    t.Logf("A == Ld*Ud: %v\n", An.AllClose(An))
+}
 
 // Local Variables:
 // tab-width: 4
