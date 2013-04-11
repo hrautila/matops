10fb1937d463ca9983ddfbff92e8af183d4b5584
Harri Rautila <harri.rautila@gmail.com>
2013-04-11 18:06:07 +0300

    Blocked LU decomposition test.

--- decomp_test.go
+++ decomp_test.go
@@ -12,20 +12,20 @@ import (
     "testing"
 )
 
-func TestLU2x2NoPiv(t *testing.T) {
+func _TestLU2x2NoPiv(t *testing.T) {
     Adata2 := [][]float64{
         []float64{4.0, 3.0},
         []float64{6.0, 3.0}}
 
     A := matrix.FloatMatrixFromTable(Adata2, matrix.RowOrder)
-    DecomposeLUnoPiv(A)
+    DecomposeLUnoPiv(A, 0)
     t.Logf("A\n%v\n", A)
     Ld := TriLU(A.Copy())
     Ud := TriU(A)
     t.Logf("L*U\n%v\n", matrix.Times(Ld, Ud))
 }
 
-func TestLU3x3NoPiv(t *testing.T) {
+func _TestLU3x3NoPiv(t *testing.T) {
     Adata2 := [][]float64{
         []float64{4.0, 2.0, 2.0},
         []float64{6.0, 4.0, 2.0},
@@ -34,14 +34,14 @@ func TestLU3x3NoPiv(t *testing.T) {
 
     A := matrix.FloatMatrixFromTable(Adata2, matrix.RowOrder)
     A0 := A.Copy()
-    DecomposeLUnoPiv(A)
+    DecomposeLUnoPiv(A, 0)
     t.Logf("A\n%v\n", A)
     Ld := TriLU(A.Copy())
     Ud := TriU(A.Copy())
     t.Logf("A == L*U: %v\n", A0.AllClose(matrix.Times(Ld, Ud)))
 }
 
-func TestLUnoPiv(t *testing.T) {
+func _TestUnblkLUnoPiv(t *testing.T) {
     N := 6
     L := matrix.FloatUniformSymmetric(N, matrix.Lower)
     U := matrix.FloatUniformSymmetric(N, matrix.Upper)
@@ -50,7 +50,23 @@ func TestLUnoPiv(t *testing.T) {
 
     A := matrix.Times(L, U)
     t.Logf("A\n%v\n", A)
-    R, err := DecomposeLUnoPiv(A.Copy())
+    R, _ := DecomposeLUnoPiv(A.Copy(), 0)
+    Ld := TriLU(R.Copy())
+    Ud := TriU(R)
+    t.Logf("A == L*U: %v\n", A.AllClose(matrix.Times(Ld, Ud)))
+}
+
+func TestBlkLUnoPiv(t *testing.T) {
+    N := 10
+    nb := 4
+    L := matrix.FloatUniformSymmetric(N, matrix.Lower)
+    U := matrix.FloatUniformSymmetric(N, matrix.Upper)
+    // Set L diagonal to 1.0
+    L.Diag().SetIndexes(1.0)
+
+    A := matrix.Times(L, U)
+    t.Logf("A\n%v\n", A)
+    R, _ := DecomposeLUnoPiv(A.Copy(), nb)
     Ld := TriLU(R.Copy())
     Ud := TriU(R)
     t.Logf("A == L*U: %v\n", A.AllClose(matrix.Times(Ld, Ud)))
