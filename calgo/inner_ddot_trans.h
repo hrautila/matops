09adb5242b166285a2b3ee7de11e3613fb766957
Harri Rautila <harri.rautila@gmail.com>
2013-05-13 15:35:41 +0300

    Minor tuning.

--- calgo/inner_ddot_trans.h
+++ calgo/inner_ddot_trans.h
@@ -14,23 +14,19 @@ void _inner_ddot_trans(double *Cr, const double *Ar, const double *Br,
                        double alpha, int nVP, int ldB)
 {
   register int k, iB;
-  register double f0, f1, f2, f3, cval;
+  register double f0, f1, f2, f3, cval0, cval1;
 
-  cval = 0.0;
+  cval0 = 0.0; cval1 = 0.0;
   // unrolling of loops;
   for (k = 0; k < nVP-3; k += 4) {
-    iB = 0;
-    f0 = Ar[0] * Br[iB];
-    cval += f0;
-    iB += ldB;
-    f1 = Ar[1] * Br[iB];
-    cval += f1;
-    iB += ldB;
-    f2 = Ar[2] * Br[iB];
-    cval += f2;
-    iB += ldB;
-    f3 = Ar[3] * Br[iB];
-    cval += f3;
+    f0 = Ar[0] * Br[0];
+    cval0 += f0;
+    f1 = Ar[1] * Br[ldB];
+    cval1 += f1;
+    f2 = Ar[2] * Br[2*ldB];
+    cval0 += f2;
+    f3 = Ar[3] * Br[3*ldB];
+    cval1 += f3;
     Br += 4*ldB;
     Ar += 4;
   }
@@ -38,25 +34,23 @@ void _inner_ddot_trans(double *Cr, const double *Ar, const double *Br,
     goto update;
 
   if (k < nVP-1) {
-    iB = 0;
     f0 = Ar[0] * Br[0];
-    cval += f0;
-    iB += ldB;
-    f1 = Ar[1] * Br[iB];
-    cval += f1;
+    cval0 += f0;
+    f1 = Ar[1] * Br[ldB];
+    cval1 += f1;
     Br += 2*ldB;
     Ar += 2;
     k += 2;
   }
   if (k < nVP) {
     f0 = Ar[0] * Br[0];
-    cval += f0;
+    cval0 += f0;
     Br += ldB;
     Ar++;
     k++;
   }
  update:
-  f0 = cval * alpha;
+  f0 = (cval0 + cval1) * alpha;
   Cr[0] += f0;
 }
 
@@ -82,24 +76,18 @@ _inner_axpy_trans(double *b0, const double *Ar, const double *Br, double alpha, 
 
   f0 = alpha * Br[0];
   for (k = 0; k < nC-3; k += 4) {
-    iB = 0;
-    b0[iB] += f0 * Ar[0];
-    iB += ldB;
-    b0[iB] += f0 * Ar[1];
-    iB += ldB;
-    b0[iB] += f0 * Ar[2];
-    iB += ldB;
-    b0[iB] += f0 * Ar[3];
+    b0[0]     += f0 * Ar[0];
+    b0[ldB]   += f0 * Ar[1];
+    b0[2*ldB] += f0 * Ar[2];
+    b0[3*ldB] += f0 * Ar[3];
     Ar += 4;
     b0 += (ldB << 2);
   }
   if (k == nC)
     return;
   if (k < nC-1) {
-    iB = 0;
-    b0[iB] += f0 * Ar[0];
-    iB += ldB;
-    b0[iB] += f0 * Ar[1];
+    b0[0]   += f0 * Ar[0];
+    b0[ldB] += f0 * Ar[1];
     Ar += 2;
     b0 += (ldB << 1);
     k += 2;
