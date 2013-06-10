02f46d7a44ca81d3d1a6016dfa7b336fbe0ee159
Harri Rautila <harri.rautila@gmail.com>
2013-06-10 19:20:13 +0300

    Solve tests.

--- qr_test.go
+++ qr_test.go
@@ -56,7 +56,7 @@ func TestDecomposeQRT(t *testing.T) {
 func TestMultQ(t *testing.T) {
     M := 60
 	N := 40
-	K := 40
+	K := 30
 	nb := 12
 	A := matrix.FloatUniform(M, N)
 	B := matrix.FloatUniform(M, K)
@@ -84,7 +84,7 @@ func TestMultQ(t *testing.T) {
 func TestMultQT(t *testing.T) {
     M := 60
 	N := 40
-	K := 40
+	K := 30
 	nb := 12
 	A := matrix.FloatUniform(M, N)
 	B := matrix.FloatUniform(M, K)
@@ -109,6 +109,95 @@ func TestMultQT(t *testing.T) {
 	nrm := NormP(B, NORM_ONE)
 	t.Logf("||B - Q*Q.T*B||_1: %e\n", nrm)
 }
+
+func TestSolveLeastSquaresQR(t *testing.T) {
+    M := 60
+	N := 40
+	K := 30
+	nb := 12
+
+	A := matrix.FloatUniform(M, N)
+	B := matrix.FloatZeros(M, K)
+	X0 := matrix.FloatUniform(N, K)
+    
+    // B = A*X0
+    Mult(B, A, X0, 1.0, 0.0, NOTRANS)
+    W := matrix.FloatZeros(N, nb)
+    tau := matrix.FloatZeros(N, 1)
+
+	QR, err := DecomposeQR(A, tau, W, nb)
+    if err != nil {
+        t.Logf("decompose error: %v\n", err)
+    }
+    // B' = A.-1*B
+    err = SolveQR(B, QR, tau, W, NOTRANS, nb)
+
+    // expect B[0:N, 0:K] == X0, B[N:M, 0:K] == 0.0
+    var Xref matrix.FloatMatrix
+    Bref := matrix.FloatZeros(M, K)
+    Bref.SubMatrix(&Xref, 0, 0, N, K)
+    Xref.Plus(X0)
+    Bref.Minus(B)
+
+    t.Logf("||B - A*X0||_1: %e\n",NormP(Bref, NORM_ONE))
+}
+
+func TestSolveLeastSquaresQRT(t *testing.T) {
+    M := 60
+	N := 40
+	K := 30
+	nb := 12
+
+	A := matrix.FloatUniform(M, N)
+	B := matrix.FloatZeros(M, K)
+	X0 := matrix.FloatUniform(N, K)
+    
+    // B = A*X0
+    Mult(B, A, X0, 1.0, 0.0, NOTRANS)
+    W := matrix.FloatZeros(N, nb)
+    T := matrix.FloatZeros(N, N)
+
+	QR, err := DecomposeQRT(A, T, W, nb)
+    if err != nil {
+        t.Logf("decompose error: %v\n", err)
+    }
+    // B' = A.-1*B
+    err = SolveQRT(B, QR, T, W, NOTRANS, nb)
+
+    // expect B[0:N, 0:K] == X0, B[N:M, 0:K] == 0.0
+    var Xref matrix.FloatMatrix
+    Bref := matrix.FloatZeros(M, K)
+    Bref.SubMatrix(&Xref, 0, 0, N, K)
+    Xref.Plus(X0)
+    Bref.Minus(B)
+    t.Logf("\nmin ||B - A*X0||\n\twhere B = A*X0\n")
+    t.Logf("||B - A*X0||_1 ~~ 0.0: %e\n",NormP(Bref, NORM_ONE))
+}
+
+func TestSolveMinQRT(t *testing.T) {
+    var QR *matrix.FloatMatrix
+    var err error
+	N := 40
+	K := 30
+	nb := 12
+
+	A := matrix.FloatUniform(N, N)
+	B0 := matrix.FloatUniform(N, K)
+    W := matrix.FloatZeros(N, nb)
+    T := matrix.FloatZeros(N, N)
+
+	QR, err = DecomposeQRT(A.Copy(), T, W, nb)
+    if err != nil {
+        t.Logf("decompose error: %v\n", err)
+    }
+    // B' = A.-1*B
+    B := B0.Copy()
+    err = SolveQRT(B, QR, T, W, TRANS, nb)
+    Mult(B0, A, B, -1.0, 1.0, TRANSA)
+    t.Logf("\nmin ||X||\n\ts.t. A.T*X = B\n\twhere A is N-by-N, B is N-by-K\n")
+    t.Logf("||B - A.T*X||_1 ~~ 0.0: %e\n",NormP(B0, NORM_ONE))
+}
+
 
 // Local Variables:
 // tab-width: 4
