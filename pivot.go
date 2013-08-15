af66c302a113eea33d4cd8cbb1aa8a2640e325f2
Harri Rautila <harri.rautila@gmail.com>
2013-08-15 23:12:15 +0300

    Symmetric pivoting.

--- pivot.go
+++ pivot.go
@@ -191,6 +191,101 @@ func applyPivotSym(AL, AR *matrix.FloatMatrix, index int, flags Flags) {
 }
 
 
+/*
+ * Apply diagonal pivot (row and column swapped) to symmetric matrix blocks.
+ * AR[0,0] is on diagonal and AL is block to the left of diagonal and AR the
+ * triangular diagonal block. Need to swap row and column.
+ *
+ * LOWER triangular; moving from top-left to bottom-right
+ *
+ *    d
+ *    x  d |
+ *    --------------------------
+ *    x  x | d 
+ *    S1 S1| S1 P1 x  x  x  P2     -- current row/col 'srcix'
+ *    x  x | x  S2 d  x  x  x
+ *    x  x | x  S2 x  d  x  x
+ *    x  x | x  S2 x  x  d  x
+ *    D1 D1| D1 P2 D2 D2 D2 P3     -- swap with row/col 'dstix'
+ *    x  x | x  S3 x  x  x  D3 d
+ *    x  x | x  S3 x  x  x  D3 x d
+ *    (ABL)          (ABR)
+ *
+ * UPPER triangular; moving from bottom-right to top-left
+ *
+ *         (ATL)                  (ATR)
+ *    d  x  x  D3 x  x  x  S3 x | x
+ *       d  x  D3 x  x  x  S3 x | x
+ *          d  D3 x  x  x  S3 x | x 
+ *             P3 D2 D2 D2 P2 D1| D1  -- dstinx
+ *                d  x  x  S2 x | x
+ *                   d  x  S2 x | x
+ *                      d  S2 x | x
+ *                         P1 S1| S1  -- srcinx
+ *                            d | x
+ *    -----------------------------
+ *                              | d
+ *                           (ABR)
+ */
+func applyPivotSym2(AL, AR *matrix.FloatMatrix, srcix, dstix int, flags Flags) {
+    var s, d matrix.FloatMatrix
+    if flags & LOWER != 0 {
+        // AL is [ABL]; AR is [ABR]; P1 is AR[0,0], P2 is AR[index, 0]
+        // S1 -- D1
+        AL.SubMatrix(&s, srcix, 0, 1, AL.Cols())
+        AL.SubMatrix(&d, dstix, 0, 1, AL.Cols())
+        Swap(&s, &d)
+        if srcix > 0 {
+            AR.SubMatrix(&s, srcix, 0, 1, srcix)
+            AR.SubMatrix(&d, dstix, 0, 1, srcix)
+            Swap(&s, &d)
+        }
+        // S2 -- D2
+        AR.SubMatrix(&s, srcix+1, srcix,   dstix-srcix-1, 1)
+        AR.SubMatrix(&d, dstix,   srcix+1, 1, dstix-srcix-1)
+        Swap(&s, &d)
+        // S3 -- D3
+        AR.SubMatrix(&s, dstix+1, srcix, AR.Rows()-dstix-1, 1)
+        AR.SubMatrix(&d, dstix+1, dstix, AR.Rows()-dstix-1, 1)
+        Swap(&s, &d)
+        // swap P1 and P3
+        p1 := AR.GetAt(srcix, srcix)
+        p3 := AR.GetAt(dstix, dstix)
+        AR.SetAt(srcix, srcix, p3)
+        AR.SetAt(dstix, dstix, p1)
+        return
+    }
+    if flags & UPPER != 0 {
+        // AL is ATL, AR is ATR; P1 is AL[srcix, srcix];
+        // S1 -- D1; 
+        AR.SubMatrix(&s, srcix, 0, 1, AR.Cols())
+        AR.SubMatrix(&d, dstix, 0, 1, AR.Cols())
+        Swap(&s, &d)
+        if srcix < AL.Cols()-1 {
+            // not the corner element
+            AL.SubMatrix(&s, srcix, srcix+1, 1, srcix)
+            AL.SubMatrix(&d, dstix, srcix+1, 1, srcix)
+            Swap(&s, &d)
+        }
+        // S2 -- D2
+        AL.SubMatrix(&s, dstix+1, srcix, srcix-dstix-1, 1)
+        AL.SubMatrix(&d, dstix,   dstix+1, 1, srcix-dstix-1)
+        Swap(&s, &d)
+        // S3 -- D3
+        AL.SubMatrix(&s, 0, srcix, dstix, 1)
+        AL.SubMatrix(&d, 0, dstix, dstix, 1)
+        Swap(&s, &d)
+        //fmt.Printf("3, AR=%v\n", AR)
+        // swap P1 and P3
+        p1 := AR.GetAt(0, 0)
+        p3 := AL.GetAt(dstix, dstix)
+        AR.SetAt(srcix, srcix, p3)
+        AL.SetAt(dstix, dstix, p1)
+        return
+    }
+}
+
+
 func ApplyRowPivots(A *matrix.FloatMatrix, ipiv []int, direction int) {
     p := &pPivots{ipiv}
     applyRowPivots(A, p, 0, direction)
