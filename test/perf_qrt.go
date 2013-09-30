ab230d1f74c024d3b35f4b3b4ff3709f614166f8
Harri Rautila <harri.rautila@gmail.com>
2013-10-01 00:24:14 +0300

    Tests for blocked QR factorization.

---
+++ test/perf_qrt.go
@@ -0,0 +1,405 @@
+
+// Copyright (c) Harri Rautila, 2012,2013
+
+// This file is part of github.com/hrautila/matops package. It is free software,
+// distributed under the terms of GNU Lesser General Public License Version 3, or
+// any later version. See the COPYING tile included in this archive.
+
+package main
+
+import (
+    "github.com/hrautila/matrix"
+    //"github.com/hrautila/linalg"
+    "github.com/hrautila/linalg/lapack"
+    "github.com/hrautila/mperf"
+    "github.com/hrautila/matops"
+    "fmt"
+    "os"
+    "io"
+    "flag"
+    "runtime"
+    "strings"
+    "strconv"
+    "math/rand"
+    "time"
+    //"unsafe"
+)
+
+
+var M, N, KB, MB, NB int
+var randomData bool
+var check bool
+var verbose bool
+var asGflops bool
+var asEps bool
+var singleTest bool
+var testUpper bool
+var refTest bool
+var noSPD bool
+var nWorker int
+var testName string
+var testCount int
+var VPsize int
+var sizeList string
+var transpose string
+var fileName string
+
+// globals for error cases
+var Tlapack, Tmatops, Rlapack, Rmatops *matrix.FloatMatrix
+var ERRlapack, ERRmatops error
+var decompOK, reflectorOK bool
+
+func init() {
+    flag.IntVar(&M, "M", 100, "Matrix rows")
+    flag.IntVar(&N, "N", 100, "Matrix cols")
+
+    // blocking size; 0 is unblocked versions
+    flag.IntVar(&KB, "KB", 0, "Blocking size for blocked invocations")
+
+    // parameters for basic matrix operations
+    flag.IntVar(&MB,      "MB", 68, "Row blocking size.")
+    flag.IntVar(&NB,      "NB", 68, "Column blocking size.")
+    flag.IntVar(&VPsize,  "H", 68, "Viewport size.")
+    flag.IntVar(&nWorker, "W", 2, "Number of workers for parallel runs")
+
+    flag.BoolVar(&singleTest, "s", false, "Run single test run for given matrix size.")
+    flag.BoolVar(&refTest,    "r", false, "Test with lapack reference function.")
+    flag.StringVar(&sizeList, "L", "",    "Comma separated list of matrix sizes.")
+    flag.IntVar(&testCount,   "n", 5,     "Number of test runs.")
+
+    flag.BoolVar(&check,      "C",   false, "Check result against lapack reference.")
+    flag.BoolVar(&verbose,    "v",   false, "Be verbose.")
+    flag.BoolVar(&asGflops,   "g",   false, "Report as Gflops.")
+    flag.StringVar(&testName, "T",   "test", "Test name for reporting")
+    flag.StringVar(&fileName, "F",   "saved.dat", "Filename for source data")
+}
+
+var sizes []int = []int{
+    10, 30, 50, 70, 90,
+    100, 200, 300, 400, 500, 600, 700, 800, 900,
+    1000, 1100, 1200, 1300, 1400, 1500}
+
+func index(i, r, sz int) int {
+    if (i == r) {
+        return sz;
+    }
+    return i*sz/r - ((i*sz/r) & 0x3);
+}
+
+
+func saveData(A *matrix.FloatMatrix) {
+    var fd *os.File
+    if fileName == "" {
+        fileName = testName + ".dat"
+    }
+    fd, err := os.Create(fileName)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "create error: %v\n", err)
+        return
+    }
+    io.WriteString(fd, A.ToString("%14e"))
+    fd.Close()
+}
+
+func checkIPIV(ipiv []int, ipiv2 []int32) bool {
+    if len(ipiv) != len(ipiv2) {
+        return false
+    }
+    for k, n := range ipiv {
+        if ipiv2[k] != int32(n) {
+            return false
+        }
+    }
+    return true
+}
+
+// single invocation for matops and lapack functions
+func runCheck(A *matrix.FloatMatrix, LB int) (bool, time.Duration, time.Duration) {
+
+    var W *matrix.FloatMatrix = nil
+    N := A.Cols()
+    T := matrix.FloatZeros(N, N)
+    if LB > 0 {
+        W = matrix.FloatZeros(A.Rows(), LB)
+    }
+    fnc := func() {
+        _, ERRmatops = matops.DecomposeQRT(A, T, W, LB)
+    }
+
+    if verbose && N < 10 {
+        fmt.Fprintf(os.Stderr, "A start:\n%v\n", A)
+    }
+    A0 := A.Copy()
+    T0 := T.Copy()
+
+    mperf.FlushCache()
+    time0 := mperf.Timeit(fnc)
+    if verbose && N < 10 {
+        //fmt.Fprintf(os.Stderr, "A end:\n%v\n", A)
+        fmt.Fprintf(os.Stderr, "T:\n%v\n", T)
+    }
+
+    fn2 := func() {
+        ERRlapack = lapack.Geqrt(A0, T0)
+    }
+
+    mperf.FlushCache()
+    time2 := mperf.Timeit(fn2)
+    if verbose && N < 10 {
+        //fmt.Fprintf(os.Stderr, "A0 end:\n%v\n", A0)
+        fmt.Fprintf(os.Stderr, "T0:\n%v\n", T0)
+    }
+   // now A == A0 && tau == tau0
+
+    decompOK = A.AllClose(A0)
+    reflectorOK = T.AllClose(T0)
+
+    if verbose {
+        if ! decompOK {
+            fmt.Fprintf(os.Stderr, "Decompose results not  equal\n")
+        }
+        if ! reflectorOK {
+            fmt.Fprintf(os.Stderr, "Block reflector results not  equal\n")
+        }
+    }
+    if ! decompOK || ! reflectorOK {
+        // save result to globals
+        Rlapack = A0
+        Rmatops = A
+        Tlapack = T0
+        Tmatops = T
+    }
+    return decompOK && reflectorOK, time0, time2
+}
+
+// 
+func runTest(A *matrix.FloatMatrix, ntest, LB int) time.Duration {
+    var W *matrix.FloatMatrix = nil
+    var mintime time.Duration
+
+    N := A.Cols()
+    tau := matrix.FloatZeros(N, 1)
+    if LB > 0 {
+        W = matrix.FloatZeros(A.Rows(), LB)
+    }
+    fnc := func() {
+        _, ERRmatops = matops.DecomposeQR(A, tau, W, LB)
+    }
+
+    A0 := A.Copy()
+    for n := 0; n < ntest; n++ {
+        if n > 0 {
+            // restore original A
+            A0.CopyTo(A)
+            tau.Scale(0.0)
+        }
+        mperf.FlushCache()
+        time0 := mperf.Timeit(fnc)
+        if n == 0 || time0 < mintime {
+            mintime = time0
+        }
+        if verbose {
+            fmt.Printf("%.4f ms\n", time0.Seconds()*1000.0)
+        }
+    }
+    return mintime
+}
+
+func runRefTest(A *matrix.FloatMatrix, ntest, LB int) time.Duration {
+
+    var mintime time.Duration
+
+    N := A.Cols()
+    tau := matrix.FloatZeros(N, 1)
+        
+    fnc := func() {
+        ERRlapack = lapack.Geqrf(A, tau)
+    }
+
+    A0 := A.Copy()
+    for n := 0; n < ntest; n++ {
+        if n > 0 {
+            // restore original A
+            A0.CopyTo(A)
+            tau.Scale(0.0)
+        }
+        mperf.FlushCache()
+        time0 := mperf.Timeit(fnc)
+        if n == 0 || time0 < mintime {
+            mintime = time0
+        }
+    }
+    return mintime
+}
+
+// create a new matrix.
+func newMatrix(M, N int) *matrix.FloatMatrix {
+    generator := &matrix.NormalFloat{0.0, 2.0}
+    A0 := matrix.FloatZeros(M, N)
+    A0.SetFrom(generator)
+    return A0
+}
+
+type testFunc func(*matrix.FloatMatrix, int, int)(time.Duration)
+
+func runTestsForSizes(test testFunc, sizes []int) map[int]float64 {
+    times := make(map[int]float64, len(sizes))
+    for _, sz := range sizes {
+        A := newMatrix(sz, sz)
+        tm := test(A, testCount, KB)
+        times[sz] = tm.Seconds()
+    }
+    return times
+}
+
+func parseSizeList(s string) []int {
+    sl := strings.Split(s, ",")
+    il := make([]int, 0)
+    for _, snum := range sl {
+        n, err := strconv.ParseInt(snum, 0, 32)
+        if err == nil {
+            il = append(il, int(n))
+        }
+    }
+    return il
+}
+
+func columnDiffs(A, B *matrix.FloatMatrix) *matrix.FloatMatrix {
+    var c matrix.FloatMatrix
+    nrm := matrix.FloatZeros(A.Cols(), 1)
+    A0 := A.Copy()
+    A0.Minus(B)
+    for k := 0; k < A.Cols(); k++ {
+        A0.SubMatrix(&c, 0, k, A.Rows(), 1)
+        nrm.SetAt(k, 0, matops.Norm2(&c))
+    }
+    return nrm
+}
+
+func rowDiffs(A, B *matrix.FloatMatrix) *matrix.FloatMatrix {
+    var r matrix.FloatMatrix
+    nrm := matrix.FloatZeros(A.Rows(), 1)
+    A0 := A.Copy()
+    A0.Minus(B)
+    for k := 0; k < A.Rows(); k++ {
+        A0.SubMatrix(&r, k, 0, 1, A.Cols())
+        nrm.SetAt(k, 0, matops.Norm2(&r))
+    }
+    return nrm
+}
+
+func gFlops(M, N int, secs float64) float64 {
+    // flops: 
+    return 2.0*float64(int64(M)*int64(N)*int64(N))/3.0/secs * 1e-9
+}
+
+func main() {
+    flag.Parse()
+    runtime.GOMAXPROCS(nWorker)
+    matops.NumWorkers(nWorker)
+    rand.Seed(time.Now().UnixNano())
+    matops.BlockingParams(MB, NB, VPsize)
+
+    var ok bool
+    var reftm, tm time.Duration
+
+    if singleTest {
+        A := newMatrix(M, N)
+        Ac := A.Copy()
+
+        if check {
+            ok, tm, reftm = runCheck(A, KB)
+            if verbose {
+                fmt.Fprintf(os.Stderr, "%s: %v\n", testName, tm)
+                fmt.Fprintf(os.Stderr, "Reference: [%v] %v (%.2f) \n",
+                    ok, reftm, tm.Seconds()/reftm.Seconds())
+                if ! ok {
+                    fmt.Fprintf(os.Stderr, "ERRlapack: %v\n", ERRlapack)
+                    fmt.Fprintf(os.Stderr, "ERRmatops: %v\n", ERRmatops)
+                }
+            }
+            if asGflops {
+                fmt.Printf("%.4f Gflops [ref: %.4f]\n",
+                    gFlops(M, N, tm.Seconds()), gFlops(M, N, reftm.Seconds()))
+            }
+            if ! ok {
+                //fmt.Printf("A orig:\n%v\n", Ac)
+                _ = Ac
+                //saveData(Ac)
+                if ! decompOK {
+                    cdf := columnDiffs(Rmatops, Rlapack)
+                    cdf.SetSize(1, cdf.Rows(), 1)
+                    fmt.Fprintf(os.Stderr, "A col diffs:\n%v\n\n", cdf)
+
+                    rdf := rowDiffs(Rmatops, Rlapack)
+                    rdf.SetSize(1, rdf.Rows(), 1)
+                    fmt.Fprintf(os.Stderr, "A row diffs:\n%v\n\n", rdf)
+                }
+
+                if ! reflectorOK {
+                    ctf := columnDiffs(Tmatops, Tlapack)
+                    ctf.SetSize(1, ctf.Rows(), 1)
+                    fmt.Fprintf(os.Stderr, "T col diffs:\n%v\n\n", ctf)
+
+                    rtf := rowDiffs(Tmatops, Tlapack)
+                    rtf.SetSize(1, rtf.Rows(), 1)
+                    if verbose {
+                        fmt.Fprintf(os.Stderr, "T row diffs:\n%v\n\n", rtf)
+                        fmt.Fprintf(os.Stderr, "T:\n%v\n", Tmatops)
+                        fmt.Fprintf(os.Stderr, "Tlapack:\n%v\n", Tlapack)
+                    }
+                }
+            }
+            return
+        }
+
+        if refTest {
+            tm = runRefTest(A, testCount, KB)
+        } else {
+            tm = runTest(A, testCount, KB)
+        }
+
+        if asGflops {
+            fmt.Printf("%.4f Gflops\n", gFlops(M, N, tm.Seconds()))
+        } else {
+            fmt.Printf("%vs\n", tm.Seconds())
+        }
+        return
+    } 
+
+
+    if len(sizeList) > 0 {
+        sizes = parseSizeList(sizeList)
+    }
+    var times map[int]float64
+
+    if refTest {
+        times = runTestsForSizes(runRefTest, sizes)
+    } else {
+        times = runTestsForSizes(runTest, sizes)
+    }
+    if asGflops {
+        if verbose {
+            fmt.Printf("calculating Gflops ...\n")
+        }
+        for sz := range times {
+            times[sz] = gFlops(sz, sz, times[sz])
+        }
+    }
+    // print out as python dictionary
+    fmt.Printf("{")
+    i := 0
+    for sz := range times {
+        if i > 0 {
+            fmt.Printf(", ")
+        }
+        fmt.Printf("%d: %v", sz, times[sz])
+        i++
+    }
+    fmt.Printf("}\n")
+}
+
+// Local Variables:
+// tab-width: 4
+// indent-tabs-mode: nil
+// End:
